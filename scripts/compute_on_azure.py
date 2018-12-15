e70cea27e438fda3390c6052cb9feca5ea275843
Enguerrand <contact@engue0774.fr>
2018-12-15 21:25:55 +0800

    master(feat/compute): Add compute script for azure cloud

---
+++ scripts/compute_on_azure.py
@@ -0,0 +1,18 @@
+from download import test as dl_test, train as dl_train
+from scripts import process_test_multithreaded, process_train
+
+
+def compute_on_azure():
+    print("[COMPUTE] [Download train dataset]")
+    dl_train.download_train()
+    print("[COMPUTE] [Download test dataset]")
+    dl_test.download_test()
+    print("[COMPUTE] [Process train dataset]")
+    process_train.process_train()
+    print("[COMPUTE] [Process test dataset]")
+    process_test_multithreaded.process_test_multithreaded()
+    print("[COMPUTE] [Finish]")
+
+
+if __name__ == '__main__':
+    compute_on_azure()
