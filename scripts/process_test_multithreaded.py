7c4bfc2c9d9a8c4eb02261dd9c56a840b50da9ae
Enguerrand <contact@engue0774.fr>
2018-12-15 21:25:36 +0800

    master(rework): Rework functions order and clean code

--- scripts/process_test_multithreaded.py
+++ scripts/process_test_multithreaded.py
@@ -5,19 +5,36 @@ from preprocess import regularts
 from pickles import pickling
 from datetime import datetime
 
+TEST_CSV_PATH = '../dataset/test_set.csv'
 CHUNK_DF_PATH_PREFIX = '../' + pickling.CHUNK_DF_FILE_PREFIX
 CHUNK_TS_DF_PATH_PREFIX = '../' + pickling.CHUNK_TS_DF_FILE_PREFIX
 CHUNK_TF_PATH_PREFIX = '../' + pickling.CHUNK_TF_FILE_PREFIX
 
 
+def process_test_csv_to_pickles():
+    chunks = 5000000  # 91 iterations require
+    for it_chunk, df_chunk in enumerate(pd.read_csv(TEST_CSV_PATH, chunksize=chunks, iterator=True)):
+        print("===================")
+        print('iteration:', it_chunk)
+        if it_chunk >= 1:
+            df_chunk = pd.concat([df_chunk_cache, df_chunk])
+        object_id_last = df_chunk.tail(1)['object_id'].values[0]
+        # remove last object_id if is not the end
+        if it_chunk != 90:
+            df_chunk_cache = df_chunk[df_chunk['object_id'] == object_id_last]
+            df_chunk = df_chunk[df_chunk['object_id'] != object_id_last]
+        pickling.pickle_chunk_dataframe(df_chunk, it_chunk)
+
+
 def process_test_multithreaded():
     start_time = datetime.now()
+    process_test_csv_to_pickles()
     test.load_apply_save_multithreaded(number_chunks=91, load_prefix=CHUNK_DF_PATH_PREFIX,
                                        save_prefix=CHUNK_TS_DF_PATH_PREFIX, function=regularts.regularize_dataframe)
     test.load_apply_save_multithreaded(number_chunks=91, load_prefix=CHUNK_TS_DF_PATH_PREFIX,
                                        save_prefix=CHUNK_TF_PATH_PREFIX, function=regularts.tensorize_regular_ts)
     time_elapsed = datetime.now() - start_time
-    print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
+    print('[Process Test Dataset] Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
 
 
 if __name__ == '__main__':
--- scripts/process_train.py
+++ scripts/process_train.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-
 from preprocess import regularts
 from pickles import pickling
+from datetime import datetime
 
 TRAIN_CSV_PATH = '../dataset/training_set.csv'
 TRAIN_TS_PATH = '../' + pickling.TRAIN_PROCESSED_DF_FILE
@@ -10,6 +10,7 @@ TRAIN_TF_PATH = '../' + pickling.TRAIN_PROCESSED_TF_FILE
 
 
 def process_train(filename=TRAIN_CSV_PATH):
+    start_time = datetime.now()
     print(f'Loading csv at {filename}')
     training_set = pd.read_csv(filename)
     print('Regularizing train TS')
@@ -20,6 +21,7 @@ def process_train(filename=TRAIN_CSV_PATH):
     tf = regularts.tensorize_regular_ts(ts)
     print(f'Pickling tensorized time series at {TRAIN_TF_PATH}')
     pickling.pickle_processed_train(tf, filename=TRAIN_TF_PATH)
+    print('[Process Train Dataset] Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
 
 
 if __name__ == "__main__":
