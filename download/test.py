f2d24ae550eb22695cda45ac90585837e7fb4edc
Enguerrand <contact@engue0774.fr>
2018-12-15 20:59:44 +0800

    master(rework): Rework script for download test an train dataset from kaggle

--- download/test.py
+++ download/test.py
@@ -1,15 +1,22 @@
 import zipfile
 import os
 
-# SCRIPT NOT WORK ON WINDOWS
-# @todo path windows
 
-os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p ../dataset')
+def download_test():
+    # SCRIPT NOT WORK ON WINDOWS
+    # @todo path windows
 
-for file in os.listdir('../dataset'):
-    print("File", file)
-    if len(file.split(".")) == 3:
-      zip_ref = zipfile.ZipFile("../dataset/" + file, 'r')
-      zip_ref.extractall("../dataset")
-      zip_ref.close()
-print("File is unzip to csv. OK.")
+    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p ../dataset -f test_set.csv')
+    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p ../dataset -f test_set_metadata.csv')
+
+    for file in os.listdir('../dataset'):
+        print("File", file)
+        if len(file.split(".")) == 3:
+          zip_ref = zipfile.ZipFile("../dataset/" + file, 'r')
+          zip_ref.extractall("../dataset")
+          zip_ref.close()
+    print("File is unzip to csv. OK.")
+
+
+if __name__ == '__main__':
+    download_test()
