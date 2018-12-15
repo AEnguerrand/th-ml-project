d176c6766bd01df0d44a71bcbd407ef223b3e9ac
Hamza Keurti <hamza.keurti@gmail.com>
2018-12-15 20:52:23 +0800

    Added implementation for multithreaded test processing.

--- load/test.py
+++ load/test.py
@@ -2,6 +2,14 @@ import pandas as pd
 from pickles import pickling
 from utils import thread_pool
 
+
+def load_apply_save(number_chunks, load_prefix, save_prefix, function, *args, **kwargs):
+    for chunk_id in range(number_chunks):
+        chunk = pickling.unpickle_chunk(chunk_id=chunk_id, prefix=load_prefix)
+        ret_chunk = function(chunk, *args, **kwargs)
+        pickling.pickle_chunk(chunk=ret_chunk, chunk_id=chunk_id, prefix=save_prefix)
+
+
 def load_apply_save_worker_func(chunk_id, load_prefix, save_prefix, function, *args, **kwargs):
     print(f"processing chunk {chunk_id}")
     chunk = pickling.unpickle_chunk(chunk_id=chunk_id, prefix=load_prefix)
@@ -11,17 +19,18 @@ def load_apply_save_worker_func(chunk_id, load_prefix, save_prefix, function, *a
     pickling.pickle_chunk(chunk=ret_chunk, chunk_id=chunk_id, prefix=save_prefix)
     print("finish", chunk_id)
 
-def load_apply_save(number_chunks, load_prefix, save_prefix, function, *args, **kwargs):
-    pool = thread_pool.ThreadPool(62)
-    for chunk_id in range(number_chunks):
-        pool.add_task(load_apply_save_worker_func, chunk_id, load_prefix, save_prefix, function, *args, **kwargs)
-    pool.wait_completion()
-
 
 def load_apply(number_chunks, load_prefix, function, *args, **kwargs):
     for chunk_id in range(number_chunks):
         chunk = pickling.unpickle_chunk(chunk_id=chunk_id, prefix=load_prefix)
         function(chunk, *args, **kwargs)
+
+
+def load_apply_save_multithreaded(number_chunks, load_prefix, save_prefix, function, *args, **kwargs):
+    pool = thread_pool.ThreadPool(62)
+    for chunk_id in range(number_chunks):
+        pool.add_task(load_apply_save_worker_func, chunk_id, load_prefix, save_prefix, function, *args, **kwargs)
+    pool.wait_completion()
 
 
 def load_pickle():
