93556e8b366b1cd0c7f20b55e195db6edcdf4c99
Hamza Keurti <hamza.keurti@gmail.com>
2018-12-15 20:34:34 +0800

    Added script for processing of train dataframe into pickles

---
+++ scripts/process_train.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+from preprocess import regularts
+from pickles import pickling
+
+TRAIN_CSV_PATH = '../dataset/training_set.csv'
+TRAIN_TS_PATH = '../' + pickling.TRAIN_PROCESSED_DF_FILE
+TRAIN_TF_PATH = '../' + pickling.TRAIN_PROCESSED_TF_FILE
+
+
+def load_train_process(filename=TRAIN_CSV_PATH):
+    print(f'Loading csv at {filename}')
+    training_set = pd.read_csv(filename)
+    print('Regularizing train TS')
+    ts = regularts.regularize_dataframe(training_set)
+    print(f'Pickling regularized time series at {TRAIN_TS_PATH}')
+    pickling.pickle_processed_train(ts,filename=TRAIN_TS_PATH)
+    print('Tensorizing time series')
+    tf = regularts.tensorize_regular_ts(ts)
+    print(f'Pickling tensorized time series at {TRAIN_TF_PATH}')
+    pickling.pickle_processed_train(tf, filename=TRAIN_TF_PATH)
+
+
+if __name__ == "__main__":
+    load_train_process()
