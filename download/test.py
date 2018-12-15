1e61d0b7eb86b67d5c651616a554f34ed46584e6
Enguerrand <contact@engue0774.fr>
2018-12-15 23:20:05 +0800

    master(fix/path): Fix path for compute script

--- download/test.py
+++ download/test.py
@@ -6,14 +6,14 @@ def download_test():
     # SCRIPT NOT WORK ON WINDOWS
     # @todo path windows
 
-    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p ../dataset -f test_set.csv')
-    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p ../dataset -f test_set_metadata.csv')
+    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p dataset -f test_set.csv')
+    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p dataset -f test_set_metadata.csv')
 
-    for file in os.listdir('../dataset'):
+    for file in os.listdir('dataset'):
         print("File", file)
         if len(file.split(".")) == 3:
-          zip_ref = zipfile.ZipFile("../dataset/" + file, 'r')
-          zip_ref.extractall("../dataset")
+          zip_ref = zipfile.ZipFile("dataset/" + file, 'r')
+          zip_ref.extractall("dataset")
           zip_ref.close()
     print("File is unzip to csv. OK.")
 
--- download/train.py
+++ download/train.py
@@ -6,13 +6,13 @@ def download_train():
     # SCRIPT NOT WORK ON WINDOWS
     # @todo path windows
 
-    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p ../dataset -f training_set.csv')
-    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p ../dataset -f training_set_metadata.csv')
-    for file in os.listdir('../dataset'):
+    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p dataset -f training_set.csv')
+    os.system('KAGGLE_USERNAME=aenguerrand KAGGLE_KEY=0f649bec92bbc12ba16e415a9e2c6b32 kaggle competitions download -c PLAsTiCC-2018 -p dataset -f training_set_metadata.csv')
+    for file in os.listdir('dataset'):
         print("File", file)
         if len(file.split(".")) == 3:
-          zip_ref = zipfile.ZipFile("../dataset/" + file, 'r')
-          zip_ref.extractall("../dataset")
+          zip_ref = zipfile.ZipFile("dataset/" + file, 'r')
+          zip_ref.extractall("dataset")
           zip_ref.close()
     print("File is unzip to csv. OK.")
 
