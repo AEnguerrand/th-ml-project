b193b82921fb0be5d374cc1898668b6d763173f5
Enguerrand <contact@engue0774.fr>
2018-12-15 23:16:45 +0800

    master(fix/opt): No download option for compute

--- scripts/compute_on_azure.py
+++ scripts/compute_on_azure.py
@@ -1,12 +1,14 @@
 from download import test as dl_test, train as dl_train
 from scripts import process_test_multithreaded, process_train
+import sys
 
 
 def compute_on_azure():
-    print("[COMPUTE] [Download train dataset]")
-    dl_train.download_train()
-    print("[COMPUTE] [Download test dataset]")
-    dl_test.download_test()
+    if len(sys.argv) >= 2 and sys.argv[1] == 'nodownload':
+        print("[COMPUTE] [Download train dataset]")
+        dl_train.download_train()
+        print("[COMPUTE] [Download test dataset]")
+        dl_test.download_test()
     print("[COMPUTE] [Process train dataset]")
     process_train.process_train()
     print("[COMPUTE] [Process test dataset]")
