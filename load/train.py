fabac615a31555fd6ef373c46e0d1e169b4dcd95
Enguerrand <contact@engue0774.fr>
2018-12-16 15:54:10 +0800

    master(rework): Rework function for load training set

--- load/train.py
+++ load/train.py
@@ -1,3 +1,19 @@
 import pandas as pd
-training_set = pd.read_csv('dataset/training_set.csv')
-training_set_metadata = pd.read_csv('dataset/training_set_metadata.csv')
+training_set = False
+training_set_metadata = False
+
+
+def load_set():
+    global training_set
+    training_set = pd.read_csv('dataset/training_set.csv')
+
+
+def load_metadata():
+    global training_set_metadata
+    print("[LOAD] Load metadata for training dataset")
+    training_set_metadata = pd.read_csv('dataset/training_set_metadata.csv')
+
+
+if __name__ == '__main__':
+    load_set()
+    load_metadata()
