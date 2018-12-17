5741da434b803a38fec536398fba380ce2e8e0a1
Enguerrand <contact@engue0774.fr>
2018-12-17 16:18:02 +0800

    master(feat): Add metadata process for all dataframe

--- features/metadata.py
+++ features/metadata.py
@@ -5,7 +5,6 @@ def features_metadata_for_object_id(object_id, datasetType='Train'):
     if datasetType == 'Train':
         res = config.training_set_metadata[config.training_set_metadata['object_id'] == object_id]
     elif datasetType == 'Test':
-        global test_set_metadata
         res = config.test_set_metadata[config.test_set_metadata['object_id'] == object_id]
     else:
         print("(features_metadata_for_object_id) Dataset not defined")
@@ -13,8 +12,20 @@ def features_metadata_for_object_id(object_id, datasetType='Train'):
     return res
 
 
-def features_metadata_usefull_for_object_id(object_id, datasetType='Train'):
+def features_metadata_useful_for_object_id(object_id, datasetType='Train'):
     metadata = features_metadata_for_object_id(object_id, datasetType)
+    del metadata['hostgal_specz'], metadata['ra'], metadata['decl'], metadata['gal_l'], metadata['gal_b'],\
+        metadata['ddf']
+    return metadata
+
+def features_metadata_useful_all(datasetType='Train'):
+    if datasetType == 'Train':
+        metadata = config.training_set_metadata
+    elif datasetType == 'Test':
+        metadata = config.test_set_metadata
+    else:
+        print("(features_metadata_for_object_id) Dataset not defined")
+        return None
     del metadata['hostgal_specz'], metadata['ra'], metadata['decl'], metadata['gal_l'], metadata['gal_b'],\
         metadata['ddf']
     return metadata
