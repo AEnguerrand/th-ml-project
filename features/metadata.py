49a9cdf418ed7fad1b15bad7b0e199d3c89c97dd
Enguerrand <contact@engue0774.fr>
2018-12-17 14:57:01 +0800

    master(feat): Get metadata (close #16)

---
+++ features/metadata.py
@@ -0,0 +1,20 @@
+from utils import config
+
+
+def features_metadata_for_object_id(object_id, datasetType='Train'):
+    if datasetType == 'Train':
+        res = config.training_set_metadata[config.training_set_metadata['object_id'] == object_id]
+    elif datasetType == 'Test':
+        global test_set_metadata
+        res = config.test_set_metadata[config.test_set_metadata['object_id'] == object_id]
+    else:
+        print("(features_metadata_for_object_id) Dataset not defined")
+        return None
+    return res
+
+
+def features_metadata_usefull_for_object_id(object_id, datasetType='Train'):
+    metadata = features_metadata_for_object_id(object_id, datasetType)
+    del metadata['hostgal_specz'], metadata['ra'], metadata['decl'], metadata['gal_l'], metadata['gal_b'],\
+        metadata['ddf']
+    return metadata
